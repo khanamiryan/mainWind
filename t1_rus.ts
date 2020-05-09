e54d0ef7f6ed1c1ec1476e528e7c2df6a02c5445
Ashot Khanamiryan <a.khanamiryan@gmail.com>
2020-05-09 18:41:03 +0400

    asdsad

---
+++ t1_rus.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="hy_AM">
+<context>
+    <name>qml</name>
+    <message>
+        <location filename="qml.qml" line="100"/>
+        <source>Ճիշտ մոլորակների քանակը</source>
+        <translatorcomment>асда</translatorcomment>
+        <translation>Правильное количество</translation>
+    </message>
+    <message>
+        <location filename="qml.qml" line="357"/>
+        <source>FATAL ERROR
+Տեղի է ունեցել սխալմունք</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qml.qml" line="364"/>
+        <source>Այոոոո</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qml.qml" line="369"/>
+        <location filename="qml.qml" line="374"/>
+        <source>Տեղի ունեցավ սխալ:
+Մուտքագրեք ճիշտ տվյալներ
+ և սեղմեք կարմիր կոճակը</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
